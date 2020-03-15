254cf2ce49864f253d2ae9ae6cf39a7fcd2d8bdc
Vinzid <vinzid@yahoo.com>
2020-03-15 11:32:00 +0800

    add history

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <div class="game">
       <div class="game-board">
-        <Board />
+        <Board :squares="history[history.length - 1].squares" @click="handleClick" />
       </div>
       <div class="game-info">
-        <div>{{ /* status */ }}</div>
+        <div>{{ status }}</div>
         <ol>{{ /* TODO */ }}</ol>
       </div>
     </div>
@@ -17,9 +17,64 @@ import Board from './components/Board.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      status: 'Next player: X'
+    }
+  },
+  methods: {
+    handleClick(i) {
+      const history = this.history;
+      const current = history[history.length - 1]
+      const squares = current.squares.slice();
+      if (calculateWinner(squares)) {
+        alert('Winner was determined!');
+        return;
+      }
+      if (squares[i]){
+        alert('Place was taken!');
+        return
+      }
+      squares[i] = this.xIsNext ? 'X' : 'O';
+      history.push({
+        squares: squares
+      });
+      const winner = calculateWinner(squares);
+      if (winner) {
+        this.status = 'Winner: ' + winner;
+        return;
+      }
+      this.xIsNext = !this.xIsNext;
+      this.status = `Next player: ${this.xIsNext ? 'X' : 'O'}`;
+    }
+  },
   components: {
     Board
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 </script>
 
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <div class="status">{{ status }}</div>
     <div class="board-row" v-for="(row, index) in board" :key="index">
       <Square v-for="square in row" :key="square" :square="square" :value="squares[square]" @click="handleClick" />
     </div>
@@ -14,60 +13,21 @@ export default {
   name: 'Board',
   data() {
     return {
-      status: 'Next player: X',
       board: [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8]
-      ],
-      squares: Array(9).fill(null),
-      xIsNext: true
+      ]
     }
   },
   methods: {
     handleClick(i) {
-      const squares = this.squares.slice();
-      if (calculateWinner(squares)) {
-        alert('Winner was determined!');
-        return;
-      }
-      if (squares[i]){
-        alert('Place was taken!');
-        return
-      }
-      squares[i] = this.xIsNext ? 'X' : 'O';
-      this.squares = squares;
-      const winner = calculateWinner(squares);
-      if (winner) {
-        this.status = 'Winner: ' + winner;
-        return;
-      }
-      this.xIsNext = !this.xIsNext;
-      this.status = `Next player: ${this.xIsNext ? 'X' : 'O'}`;
+      this.$emit('click', i);
     }
   },
+  props: ['squares'],
   components: {
     Square
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 </script>
